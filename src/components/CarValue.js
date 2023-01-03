502f23640a261af30ceb715e1b289984306a34f2
Amit Kumar Das <amit.inverse@gmail.com>
2023-01-03 21:55:34 +0530

    Total Car Cost

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,5 +1,21 @@
+import { useSelector } from 'react-redux';
+
 function CarValue() {
-    return <div>CarValue</div>;
+    // const totalCost = useSelector(({ cars: { data, searchTerm } }) => {
+    //     const filteredCars = data.filter((car) => {
+    //         return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+    //     });
+    //     // let cost = 0;
+    //     // for (let car of filteredCars) {
+    //     //     cost += car.cost;
+    //     // }
+    //     // return cost;
+
+    //     return filteredCars.reduce((acc, car) => acc + car.cost, 0);
+    // });
+    const totalCost = useSelector(({ cars: { data, searchTerm } }) => data.filter((car) => car.name.toLowerCase().includes(searchTerm.toLowerCase())).reduce((acc, car) => acc + car.cost, 0));
+
+    return <div className="car-value">Total Cost: ${totalCost}</div>;
 }
 
 export default CarValue;
